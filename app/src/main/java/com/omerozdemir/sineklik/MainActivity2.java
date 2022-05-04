9543161158f0d4ada5cec9a95967498d93fc6081
omerozdemir58 <omerozdemirr58@gmail.com>
2022-05-05 00:01:06 +0300

    K Çoğaltılacak2

--- app/src/main/java/com/omerozdemir/sineklik/MainActivity2.java
+++ app/src/main/java/com/omerozdemir/sineklik/MainActivity2.java
@@ -10,18 +10,29 @@ import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-TextView textView;
+TextView textView1;
+    TextView textView2;
 SharedPreferences sharedPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        textView = findViewById(R.id.textView3);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
         sharedPreferences= this.getSharedPreferences("com.omerozdemir.sineklik", Context.MODE_PRIVATE);
 
         Intent intent = getIntent();
         String result =  sharedPreferences.getString("result2","");
-        textView.setText("Ölçü: " +result);
+        if (textView1.getText().toString().matches("")){
+            Intent intent1 = getIntent();
+            String result1 =  sharedPreferences.getString("result2","");
+            textView1.setText("Ölçü: " + result);
+        }else {
+            Intent intent2 = getIntent();
+            String result2 =  sharedPreferences.getString("result2","");
+            textView2.setText("Ölçü: " + result);
+
+        }
 
 
 
